0a586371d4a3eabeba583b424e185d16a0382994
KarolineHuth <55590854+KarolineHuth@users.noreply.github.com>
2021-05-03 15:04:42 +0200

    Add files via upload
    data generation update and power inclusion

--- R:/AuxiliaryFunctions.R
+++ R:/AuxiliaryFunctions.R
@@ -1,65 +1,62 @@
-#### GGM Data Simulation ####
-
+library(corpcor)
 GGMSimulationSplit <- function(p, n, prob, delta_interaction){
   
   nedges <- p*(p-1)/2 # number of edges
   
   posdef <- FALSE
-  
   while(posdef == FALSE) {
     # generate random network with x nodes, simulate pcor
-    graph <- GeneNet::ggm.simulate.pcor(p, etaA=prob)
-    diag(graph) <- 1
+    graph <- GeneNet::ggm.simulate.pcor(p, etaA = prob)
+    sigma <- corpcor::pcor2cor(graph)
     
     if (delta_interaction == 0) {
       
       # Simulate only one big dataset in case of no correlation difference
-      pcor.inv <- corpcor::pseudoinverse(graph)
       mean <- rep(0, p)
       
-      d <- mvtnorm::rmvnorm(n, mean = mean, sigma = pcor.inv)
+      d <- as.data.frame(mvtnorm::rmvnorm(n, mean = mean, sigma = sigma))
       posdef <- TRUE
     } else {
       
-      # If in variable correlation setting, modify the partial correlation matrix
-      change <- sample(0:1, size = nedges, prob = c(1-prob, prob), replace  = TRUE)
-      change[change == 1] <- sample(c(+ delta_interaction, - delta_interaction), size = sum(change), replace = TRUE)
-      
-      #Interaction Matrix
-      graph2 <- matrix(0, p, p)
-      graph2[lower.tri(graph2)] <- change
-      graph2[upper.tri(graph2)] <- t(graph2)[upper.tri(graph2)] 
-      graph2 <- graph + graph2
+      sigma2 <- sigma
+      # to avoid correlation above |1| 
+      modVals <- c(sigma2[1, 2] + delta_interaction, sigma2[1, 2] - delta_interaction)
+      modVal <- modVals[which.min(abs(modVals))]
+      sigma2[1, 2] <- sigma2[2, 1] <- modVal
       
       
-      if (!corpcor::is.positive.definite(graph2, tol=1e-8)){
-        posdef <- FALSE
-      } else {
-        posdef <- TRUE
-      }
-      
-      pcor.inv <- corpcor::pseudoinverse(graph)
-      pcor.inv2 <- corpcor::pseudoinverse(graph2)
-      mean <- rep(0, p)
-      
-      # Sample Data 
-      d <- rbind(
-        mvtnorm::rmvnorm(n/2, mean = mean,
-                         sigma = pcor.inv),
-        mvtnorm::rmvnorm(n/2, mean = mean,
-                         sigma = pcor.inv2)
-      )
+      #if(all(eigen(graph)$values > 0) & all(eigen(graph2)$values > 0))
+      # if (!corpcor::is.positive.definite(sigma2, tol=1e-8)){
+      #   posdef <- FALSE
+      # } else {
+        mean <- rep(0, p)
+        sigma2 <- as.matrix(Matrix::nearPD(sigma2)$mat)
+        # Sample Data 
+        data1 <- try(mvtnorm::rmvnorm(n/2, mean = mean,
+                                      sigma = sigma))
+        data2 <- try( mvtnorm::rmvnorm(n/2, mean = mean,
+                                       sigma = sigma2))
+        
+        
+        if(!inherits(data1, "try-error") & !inherits(data2, "try-error")) {
+          posdef <- TRUE
+          d <- as.data.frame(rbind(data1, data2))
+        }
+      # }
     }
-    
   }
   
   # Generate a splitting variable/moderator variable
-  z1 <- sort(sample(18:75, size = n, replace = TRUE)) # continuous splitting variable
+  #z1 <- sort(sample(18:75, size = n, replace = TRUE)) # continuous splitting variable
+  z1 <- sort(rnorm(n)) 
   #z1 <- c(rep(0, n/2), rep(1, n/2)) # binary splitting variable
   
   data <- cbind(as.data.frame(z1) , d)
   
   colnames(data)[2:(1+p)] <- paste0("y", 1:p)
   
-  return(data)
+  return(as.data.frame(data))
+  #return(list(data, graph, graph2))
 }
+#data <- GGMSimulationSplit(p = 5, n = 500, prob = 0.5, delta_interaction = .5)
+
--- R:/StrucChange - Resampling - GGM.R
+++ R:/StrucChange - Resampling - GGM.R
@@ -1,4 +1,4 @@
-# rm(list = ls())
+#rm(list = ls())
 library("readr")
 library("networktree")
 library("MASS")
@@ -18,56 +18,6 @@ permutationData <- function(origdata){
   return(newdata)
 }
 
-
-# ----------------------------------------------------------------------
-# Computing structural change test
-
-strucchangeGGM <- function(process, splitvar, model = "all"){
-  
-  k <- NCOL(process)
-  n <- NROW(process)
-  nobs <- n 
-  
-  ## scale process
-  process <- process/sqrt(nobs)
-  meat <- crossprod(process)
-  J12 <- root.matrix(chol2inv(chol(meat)))
-  
-  process <- t(J12 %*% t(process))  
-  
-  ## Order along splitting variable
-  zi <- as.matrix(splitvar)
-  oi <- order(zi)
-  proci <- process[oi, , drop = FALSE]
-  
-  ## order partitioning variable
-  zi <- zi[oi]
-  # re-apply factor() added to drop unused levels
-  zi <- factor(zi, levels = unique(zi))
-  
-  segweights <- table(zi)
-  segweights <- as.vector(segweights)/nobs
-  
-  proci <- apply(proci, 2L, cumsum)
-  tt0 <- head(cumsum(table(zi)), -1L)
-  tt <- head(cumsum(segweights), -1L)
-  
-  proci <- rowSums(proci^2)
-  stat <- max(proci[tt0] / (tt * (1-tt)))
-  
-  return(stat)
-}
-
-## Test function
-# Generate a dataset
-# origdata <- data.frame(id = 1:500, GGMSimulationSplit(p = 5, n = 500, prob = .2, delta_interaction = 0))
-# nodevars <- as.data.frame(origdata[, 3:7])
-# splitvar <- as.data.frame(origdata[, 2])
-# splitvars <- as.data.frame(as.factor(origdata[, 2]))
-# 
-# strucchangeGGM(nodevars, splitvar, model = "correlation")
-
-
 # ------------------------------------------------------------
 # Obtain p-value trough permutation
 
@@ -78,49 +28,71 @@ permutationPval <- function(i, p, n, deltacor = 0, permiter){
     # Generate Original Dataset
     
     origdata <- data.frame(id = 1:n, 
-                           GGMSimulationSplit(p = p, n = n, prob = .2, 
+                           GGMSimulationSplit(p = p, n = n, prob = .5, 
                                               delta_interaction = deltacor))
     nodevars <- as.data.frame(origdata[, 3:(p+2)])
-    process <- networktree::mvnfit(nodevars, estfun = TRUE)$estfun
+    #process <- networktree::mvnfit(nodevars, estfun = TRUE)$estfun
     
-    splitvar <- as.data.frame(origdata[, 2])
-    
-    orig_stat <- try(strucchangeGGM(process, splitvar), silent = TRUE)
-    if(!inherits(orig_stat, "try-error")) estim <- TRUE
+    splitvar <- origdata[, 2]
+    splitvars <- as.data.frame(splitvar)
+    #MOB direct
+    minsplit <- p*(p-1)/2 + 1
+    if(minsplit > n/2) {minsplit <- (n/2 -10)}
+    ## MOB
+    out <-  try(networktree::networktree(nodevars = nodevars, splitvars = splitvars, method = c("mob"), model = "correlation",
+                                        verbose = F, minsplit = minsplit, maxdepth = 2))
+
+    ## own Function 
+    #out <- try(strucchangeGGM(process, splitvar, minsplit), silent = TRUE)
+    if(!inherits(out, "try-error")) {
+      estim <- TRUE
+      ## MOB
+      orig_stat <- unlist(out)$node.info.test1
+      ## own function
+      #orig_stat <- out
+    }
   }
   # Resample from that original dataset
   out_stat <- numeric(permiter)
   
-  for(i in 1:permiter) {
+  for(f in 1:permiter) {
     run <- FALSE
     while(run == FALSE){
       permdata <- permutationData(origdata)
-      permsplitvar <- as.data.frame(permdata[, 2])
+      permsplitvar <- permdata[, 2]
+      permsplitvars <- as.data.frame(permsplitvar)
       
-      out <- try(strucchangeGGM(process, splitvar = permsplitvar), silent = TRUE)
+      out <-  try(networktree::networktree(nodevars = nodevars, splitvars = permsplitvars, method = c("mob"), model = "correlation",
+                                           verbose = F, minsplit = minsplit, maxdepth = 2))
+      #out <- try(strucchangeGGM(process, splitvar = permsplitvar, minsplit), silent = TRUE)
       if(!inherits(out, "try-error")) run <- TRUE
     }
-    out_stat[i] <- out
+    #out_stat[f] <- out
+    out_stat[f] <- unlist(out)$node.info.test1
   }
   
   # Determine p-value
-  pval <- mean(out_stat > orig_stat)
+  pval <- mean(out_stat >= orig_stat)
   
-  res <- list(p = p, n = n, cor = deltacor, pval = pval)
+  if(pval == 0){
+    pval <- 1/permiter
+  }
+  
+  res <- list(p = p, n = n, cor = deltacor, pval = pval, testStat = orig_stat)
   
   return(res)
 }
 
-#permutationPval(1, p = 5, n = 250, bootiter  = 100)
+# permutationPval(1, p = 10, n = 200, deltacor = 0.1,  permiter  = 1000)
 
 #----------------------------------------------------------------------
 # Simulations Regenerate the p-value using resampling
 
 n <- c(200, 500, 2000) # sample size
 p <- c(5, 10, 15) # size of network
-repiter <- 5000 # number of repetitions 
-permiter <- 1000 # number of samples for permutation approach
-cor <- 0 # measurement invariance violation (set to 0 to simulate data under the null - no measurement invariance violation)
+repiter <- 1000 # number of repetitions 
+permiter <- 5000 # number of samples for permutation approach
+cor <- c(0.1, 0.3, 0.5) # measurement invariance violation (set to 0 to simulate data under the null - no measurement invariance violation)
 numCores <- detectCores()
 cntr <- 0
 res_GGMPerm <- list()
@@ -135,13 +107,29 @@ for (pi in 1:length(p)) {
       res_GGMPerm[[cntr]] <- mclapply(1:repiter, permutationPval, p = pit, n = nit,
                                    deltacor = cit,
                                    permiter = permiter, mc.cores = numCores)
+      
+      ## Printing intermediate results
+      out <- as.numeric(unlist(res_GGMPerm))
+      res <- na.omit(out)
+      pvalPermutationGGM <- as.data.frame(matrix(res, ncol = 5, byrow = T))
+      colnames(pvalPermutationGGM) <- c("p", "n", "cor", "pval", "testStat")
+      intermediate_res <- pvalPermutationGGM %>%
+        filter(cor != 0) %>%
+        mutate(sig = ifelse(pval < 0.05, 1L, 0L)) %>% 
+        group_by(n, p, cor) %>% 
+        na.omit() %>%
+        dplyr::summarize(count_all = n(),
+                         power = mean(sig, na.rm = TRUE))
+      print(intermediate_res)
     }
+    print(nit)
   }
+  print(pit)
 }
 
 out <- as.numeric(unlist(res_GGMPerm))
 res <- na.omit(out)
-pvalPermutationGGM <- as.data.frame(matrix(res, ncol = 4, byrow = T))
-colnames(pvalPermutationGGM) <- c("p", "n", "cor", "pval")
-write.csv(pvalPermutationGGM, "pval-Permutation-GGM-cont.csv")
-
+pvalPermutationGGM <- as.data.frame(matrix(res, ncol = 5, byrow = T))
+colnames(pvalPermutationGGM) <- c("p", "n", "cor", "pval", "testStat")
+write.csv(pvalPermutationGGM, "power-Permutation-GGM-cont.csv")
+head(pvalPermutationGGM)
